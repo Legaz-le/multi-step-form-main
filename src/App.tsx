d4462876ce536c0e933771bd7f01247acee2f26d
Legaz <worldoflegaz@icloud.com>
2025-08-23 20:13:46 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="flex item-center">sdadasdsadsa</div>
+    </div>
+  );
+};
+
+export default App;
