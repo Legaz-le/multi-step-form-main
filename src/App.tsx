de8d1e2ec93e0767f6bc3eb0f02beeac37486d7a
Legaz <worldoflegaz@icloud.com>
2025-08-31 17:14:36 +0900

    added images and basic tools

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 const App = () => {
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <div className="flex item-center"></div>
+    <div className="flex justify-center items-center min-h-screen bg-Blue-200">
+      <div className="flex item-center bg-White p-5 max-w-3xl w-3xl  rounded-lg shadow-lg">
+        <div className=""></div>
+      </div>
     </div>
   );
 };
