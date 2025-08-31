bc1ce4a7ad096ada00ee6ea36a22028064eb84df
Legaz <worldoflegaz@icloud.com>
2025-08-31 23:02:52 +0900

    basic background

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 const App = () => {
   return (
     <div className="flex justify-center items-center min-h-screen bg-Blue-200">
-      <div className="flex item-center bg-White p-5 max-w-3xl w-3xl  rounded-lg shadow-lg">
-        <div className=""></div>
+      <div className="flex item-center bg-White p-5 max-w-5xl w-5xl  rounded-lg shadow-lg">
+        <div className="flex-1">
+          <img src="/public/images/bg-sidebar-desktop.svg"></img>
+        </div>
+        <div className="flex-1">
+          <div className="pt-5 space-y-1">
+            <h1 className="text-2xl text-Blue-950 font-[700]">Personal info</h1>
+            <p className="text-[16px]">Please provide your name, email address, and phone number.</p>
+          </div>
+        </div>
       </div>
     </div>
   );
