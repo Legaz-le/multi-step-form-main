689418d3a585354a034c80fd9d0fb45d22a63154
Legaz <worldoflegaz@icloud.com>
2025-08-31 23:34:08 +0900

    added form and button

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,54 @@
 const App = () => {
   return (
     <div className="flex justify-center items-center min-h-screen bg-Blue-200">
-      <div className="flex item-center bg-White p-5 max-w-5xl w-5xl  rounded-lg shadow-lg">
-        <div className="flex-1">
+      <div className="flex item-center bg-White p-5 max-w-4xl w-full  rounded-lg shadow-lg space-x-20">
+        <div className="">
           <img src="/public/images/bg-sidebar-desktop.svg"></img>
         </div>
-        <div className="flex-1">
+        <div className="">
           <div className="pt-5 space-y-1">
             <h1 className="text-2xl text-Blue-950 font-[700]">Personal info</h1>
-            <p className="text-[16px]">Please provide your name, email address, and phone number.</p>
+            <p className="text-[16px] text-Grey-500 font-[400]">
+              Please provide your name, email address, and phone number.
+            </p>
           </div>
+          <form className="">
+            <div>
+            <label className="block pt-5">
+              <p className="text-Blue-900 font-[500] text-[14px] pt-5 pb-1">
+                Name
+              </p>
+              <input
+                type="text"
+                placeholder="e.g. Stephen King"
+                className="border border-Purple-200 rounded-md p-2 w-full "
+              />
+            </label>
+            <label className="block pt-">
+              <p className="text-Blue-900 font-[500] text-[14px] pt-5 pb-1">
+                Email Address
+              </p>
+              <input
+                type="email"
+                placeholder="e.g. stephenking@lorem.com"
+                className="border border-Purple-200 rounded-md p-2 w-full "
+              />
+            </label>
+            <label className="block pt-">
+              <p className="text-Blue-900 font-[500] text-[14px] pt-5 pb-1">
+                Phone Number
+              </p>
+              <input
+                type="tel"
+                placeholder="e.g. +1 234 567 890"
+                className="border border-Purple-200 rounded-md p-2 w-full "
+              />
+            </label>
+            </div>
+            <button className="mt-10 bg-Blue-950 text-White font-[500] py-3 px-7 rounded-md hover:bg-Blue-900">
+              Next Step
+            </button>
+          </form>
         </div>
       </div>
     </div>
