236b0d2700cde6e804b509d1ba905e18e33ecabf
Legaz <worldoflegaz@icloud.com>
2025-09-03 23:37:45 +0900

    added side stages

--- src/components/steps.tsx
+++ src/components/steps.tsx
@@ -4,7 +4,28 @@ const StepsProcess = () => {
       <img src="/public/images/bg-sidebar-desktop.svg" />
       <div className="absolute top-10 left-10 spacye-y-8">
         <div className="flex items-center justify-center">
-        <span className="text-White">1</span>
+          <div className="w-8 h-8 rounded-full flex items-center jutify-center border-1 border-White ">
+            <span className="text-White ml-2">1</span>
+          </div>
+          <span className="text-Grey-500 ml-4 text-[13px] font-[400]">STEP 1</span>
+        </div>
+        <div className="flex items-center justify-center pt-8">
+          <div className="w-8 h-8 rounded-full flex items-center jutify-center border-1 border-White">
+            <span className="text-White ml-2">2</span>
+          </div>
+          <span className="text-Grey-500 ml-4 text-[13px] font-[400]">STEP 2</span>
+        </div>
+        <div className="flex items-center justify-center pt-8">
+          <div className="w-8 h-8 rounded-full flex items-center jutify-center border-1 border-White">
+            <span className="text-White ml-2">3</span>
+          </div>
+          <span className="text-Grey-500 ml-4 text-[13px] font-[400]">STEP 3</span>
+        </div>
+        <div className="flex items-center justify-center pt-8">
+          <div className="w-8 h-8 rounded-full flex items-center jutify-center border-1 border-White">
+            <span className="text-White ml-2">4</span>
+          </div>
+          <span className="text-Grey-500 ml-4 text-[13px] font-[400]">STEP 4</span>
         </div>
       </div>
     </div>
