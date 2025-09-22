b36a44b21ce410dbe3d1472c4ed58f2280dd387c
Legaz <188694281+Legaz-le@users.noreply.github.com>
2025-09-23 01:04:30 +0900

    fixed issue with src image

--- src/components/common/BodyOfForms/Confrimpage.tsx
+++ src/components/common/BodyOfForms/Confrimpage.tsx
@@ -1,7 +1,7 @@
 const Confrimpage = () => {
   return (
     <div className="flex flex-col items-center justify-center text-center space-y-6">
-      <img src="/public/images/icon-thank-you.svg" alt="thank-icon" />
+      <img src="/images/icon-thank-you.svg" alt="thank-icon" />
       <div className="space-y-3">
       <h3 className="text-2xl text-Blue-950 font-[700]">Thank you!</h3>
       <p className="text-Grey-500 font-[400]">
